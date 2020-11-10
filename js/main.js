2763538561f3684a6f3ac4e4736f278f08f9fee8
KateBorisova <kathrin.borisova@gmail.com>
2020-11-10 21:01:18 +0100

    добавлено перемещение mapPinMain

--- js/main.js
+++ js/main.js
@@ -31,11 +31,75 @@
     window.ajax.getPins(onPinsFetchSuccess, onPinsFetchError);
   };
 
-  let onMouseDown = function (evt) {
-    if (evt.button === 0) {
-      enablePage();
+  let mainMap = document.querySelector(`.map`);
+  let mapPinMain = document.querySelector(`.map__pin--main`);
+  const MAIN_PIN_HEIGHT = 65;
+  const MAIN_PIN_POINTER_HEIGHT = 22;
+  const MAIN_PIN_WIDTH = 65;
+  const DRAG_LIMIT = {
+    X: {
+      MIN: 0,
+      MAX: mainMap.clientWidth
+    },
+    Y: {
+      MIN: 130,
+      MAX: 630
     }
   };
+
+  let pinClickCoordinates = {};
+
+  let onMouseMove = function (evt) {
+    evt.preventDefault();
+
+    let mainPinPosition = {
+      x: evt.pageX - pinClickCoordinates.x - mainMap.offsetLeft,
+      y: evt.pageY - pinClickCoordinates.y - mainMap.offsetTop
+    };
+    let Border = {
+      TOP: DRAG_LIMIT.Y.MIN - mapPinMain.offsetHeight - MAIN_PIN_POINTER_HEIGHT,
+      BOTTOM: DRAG_LIMIT.Y.MAX - mapPinMain.offsetHeight - MAIN_PIN_POINTER_HEIGHT,
+      LEFT: DRAG_LIMIT.X.MIN,
+      RIGHT: DRAG_LIMIT.X.MAX - mapPinMain.offsetWidth
+    };
+
+    if (mainPinPosition.x >= Border.LEFT && mainPinPosition.x <= Border.RIGHT) {
+      mapPinMain.style.left = mainPinPosition.x + `px`;
+    }
+    if (mainPinPosition.y >= Border.TOP && mainPinPosition.y <= Border.BOTTOM) {
+      mapPinMain.style.top = mainPinPosition.y + `px`;
+    }
+    let pinTailCoords = {
+      x: mainPinPosition.x + Math.ceil(MAIN_PIN_WIDTH / 2),
+      y: mainPinPosition.y + MAIN_PIN_HEIGHT + MAIN_PIN_POINTER_HEIGHT
+    };
+
+    window.form.setAddressInputValue(
+        pinTailCoords.x,
+        pinTailCoords.y
+    );
+  };
+
+  let onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    mainMap.removeEventListener(`mousemove`, onMouseMove);
+    mainMap.removeEventListener(`mouseup`, onMouseUp);
+    enablePage();
+  };
+
+  let onMouseDown = function (evt) {
+    evt.preventDefault();
+    if (evt.button === 0) {
+      pinClickCoordinates = {
+        x: evt.offsetX,
+        y: evt.offsetY
+      };
+      mainMap.addEventListener(`mousemove`, onMouseMove);
+      mainMap.addEventListener(`mouseup`, onMouseUp);
+    }
+  };
+
   let onKeyDown = function (evt) {
     if (evt.key === `Enter`) {
       enablePage();
